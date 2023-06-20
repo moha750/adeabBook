47156a824297cd92e343f2fea22302aa6100760d
moha750 <104118616+moha750@users.noreply.github.com>
2023-06-20 20:02:19 +0300

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+ var loader = document.getElementById("preloader");
+
+ window.addEventListener("load", function(){
+   loader.style.display = "none";
+});
+
 var pages = document.getElementsByClassName('page');
   for(var i = 0; i < pages.length; i++)
     {
